53ca33309b94ce7a138a20343c9b8fe571fa06c3
Aymen LAOUINI <aymen.laouini@nordicsemi.no>
2024-10-28 17:56:19 +0200

    [NRFX-6558] Update device info service client side to handle new data format model for UUID req and resp

--- include/sdfw/sdfw_services/device_info_service.h
+++ include/sdfw/sdfw_services/device_info_service.h
@@ -8,19 +8,23 @@
 #define DEVICE_INFO_SERVICE_H__
 
 #include <stddef.h>
+#include <stdint.h>
 
 #include <sdfw/sdfw_services/ssf_errno.h>
 
 /** .. include_startingpoint_device_info_header_rst */
 /**
  * @brief       Read UUID value.
+ * 
+ * @note        UUID is provided in words, byte order is the same as 
+ *              read from the memory and no endianness swapping is performed.
  *
- * @param[out]  str_out Local buffer for copying the UUID c string into.
- * @param[in]   str_out_size Size of local buffer.
+ * @param[out]  uuid_words Local buffer for copying the UUID words into.
+ * @param[in]   uuid_words_count Size of local buffer, should be at least 4 words.
  *
  * @return      0 on success, otherwise a negative errno.
  */
-int ssf_device_info_get_uuid(char *str_out, size_t str_out_size);
+int ssf_device_info_get_uuid(uint32_t* uuid_words, size_t uuid_words_count);
 
 /** .. include_endpoint_device_info_header_rst */
 
--- subsys/sdfw_services/services/device_info/device_info_service.c
+++ subsys/sdfw_services/services/device_info/device_info_service.c
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-/** .. include_startingpoint_echo_source_rst */
+/** .. include_startingpoint_device_info_source_rst */
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
@@ -17,76 +17,69 @@
 #include <sdfw/sdfw_services/ssf_client.h>
 #include "ssf_client_os.h"
 
+#define UUID_BYTES_LENGTH   16UL
+
 SSF_CLIENT_LOG_REGISTER(device_info_service, CONFIG_SSF_DEVICE_INFO_SERVICE_LOG_LEVEL);
 
 SSF_CLIENT_SERVICE_DEFINE(device_info_srvc, DEVICE_INFO, cbor_encode_device_info_req,
 			  cbor_decode_device_info_resp);
 
-enum {
-	UUID = 0,
-	HWVERSION,
-} device_info_target;
 
-enum {
-	SUCCESS = 0,
-	INTERNAL_ERROR = 0x1001001,
-	UNPROGRAMMED = 0x1001002,
-} device_info_status;
-
-
-int ssf_device_info_get_uuid(char *str_out, size_t str_out_size)
+int ssf_device_info_get_uuid(uint32_t* uuid_words, size_t uuid_words_count)
 {
 	int err = -1; /* initialize with negative value for error */
-	int ret = -1; /* initialize with negative value for error */
-	const uint8_t *rsp_pkt; /* For freeing response packet after copying rsp_str. */
 
-	struct device_info_service_read_req uuid_read_req = { 
-			.device_info_service_read_req_target = UUID,
+	if ((NULL != uuid_words) && (uuid_words_count >= UUID_BYTES_LENGTH)) {
+		// valid parameters
+		int ret = -1; /* initialize with negative value for error */
+		const uint8_t *rsp_pkt; /* For freeing response packet after copying rsp_str. */
+
+		struct device_info_service_read_req uuid_read_req = {
+				.device_info_service_read_req_target.device_info_target_choice = device_info_target_UUID_c,
+			};
+
+		struct device_info_req read_req = {
+				.device_info_req_msg_choice = device_info_req_msg_device_info_service_read_req_m_c,
+				.device_info_req_msg_device_info_service_read_req_m = uuid_read_req
 		};
 
-	struct device_info_req read_req = {
-			.device_info_req_msg_choice = device_info_req_msg_device_info_service_read_req_m_c,
-			.device_info_req_msg_device_info_service_read_req_m = uuid_read_req
-	};
+		struct device_info_resp read_resp;
 
-	struct device_info_resp read_resp;
-
-	err = ssf_client_send_request(&device_info_srvc, &read_req, &read_resp, &rsp_pkt);
-	if (err != 0) {
-		return err;
-	}
-
-	struct device_info_service_read_resp uuid_read_resp = read_resp.device_info_resp_msg_device_info_service_read_resp_m;
-	if (read_resp.device_info_resp_msg_choice == device_info_resp_msg_device_info_service_read_resp_m_c) {
-		if(UUID == uuid_read_resp.device_info_service_read_resp_target) {
-			ret = read_resp.device_info_resp_msg_device_info_service_read_resp_m.device_info_service_read_resp_status;
-			if (SUCCESS != ret) {
-				ssf_client_decode_done(rsp_pkt);
-				return ret;
-			}
-		} else {
-			// not a read UUID response
-			ssf_client_decode_done(rsp_pkt);
-			return INTERNAL_ERROR;
+		err = ssf_client_send_request(&device_info_srvc, &read_req, &read_resp, &rsp_pkt);
+		if (err != 0) {
+			return err;
 		}
 
-	} else {
-		// not a read response
+		struct device_info_service_read_resp uuid_read_resp = read_resp.device_info_resp_msg_device_info_service_read_resp_m;
+		if (read_resp.device_info_resp_msg_choice == device_info_resp_msg_device_info_service_read_resp_m_c) {
+			if(device_info_target_UUID_c == uuid_read_resp.device_info_service_read_resp_target.device_info_target_choice) {
+				ret = read_resp.device_info_resp_msg_device_info_service_read_resp_m.device_info_service_read_resp_status.device_info_status_choice;
+				if (device_info_status_SUCCESS_c != ret) {
+					ssf_client_decode_done(rsp_pkt);
+					return ret;
+				}
+			} else {
+				// not a read UUID response
+				ssf_client_decode_done(rsp_pkt);
+				return device_info_status_INTERNAL_ERROR_c;
+			}
+
+		} else {
+			// not a read response
+			ssf_client_decode_done(rsp_pkt);
+			return device_info_status_INTERNAL_ERROR_c;
+		}
+
+		size_t uuid_words_len = uuid_read_resp.device_info_service_read_resp_data_uint_count;
+		memcpy(uuid_words, uuid_read_resp.device_info_service_read_resp_data_uint, uuid_words_len * sizeof(uint32_t));
+
 		ssf_client_decode_done(rsp_pkt);
-		return INTERNAL_ERROR;
+	} else {
+		// invalid parameters
+		err = -1;
+
 	}
-
-
-	const uint8_t *uuid_str;
-	size_t uuid_str_len;
-
-	uuid_str = uuid_read_resp.device_info_service_read_resp_data.value;
-	uuid_str_len = uuid_read_resp.device_info_service_read_resp_data.len;
-	memcpy(str_out, uuid_str, SSF_CLIENT_MIN(str_out_size, uuid_str_len));
-
-	ssf_client_decode_done(rsp_pkt);
-
-	return 0;
+	return err;
 }
 
 /** .. include_endpoint_device_info_source_rst */
